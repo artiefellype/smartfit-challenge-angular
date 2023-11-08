41d700807db9f7bc154fac10e077100813d0d5ab
artiefellype <arthurfellype21@gmail.com>
2023-11-07 22:22:17 -0300

    <Feat>[Form]: Added forms

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit{
+
+  results = [];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+}
